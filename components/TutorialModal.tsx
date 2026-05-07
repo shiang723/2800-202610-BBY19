c537ef0d714d5cdbfa3750e1f816bf2e78e9c3fc
shiang723 <hanhanshiang6@gmail.com>
2026-05-06 21:14:46 -0700

    feat:  Added a Welcome Tutorial component

--- components/TutorialModal.tsx
+++ components/TutorialModal.tsx
@@ -1,28 +1,72 @@
-import Image from "next/image";
-export default function TutorialModal({ header, icon, nextLocation, lastLocation, message, first, last }: {
-    header: string,
-    icon: string,
-    nextLocation: string,
-    message: string,
-    lastLocation: string,
-    first: string,
-    last: string
-}) {
+'use client'
 
-    const backButtonClass = "bg-gray-200 hover:bg-gray-300 text-black rounded-lg p-2 pl-4 pr-4 mt-5" + ((first === "true") ? " hidden" : "");
-    const nextButtonText = (last === "true") ? "Done" : "Next";
+import Image from "next/image";
+import { useRef } from "react";
+
+interface TutorialModalProps {
+    id: string;
+    header: string;
+    icon: string;
+    iconHeight?: string;
+    iconWidth?: string;
+    nextLocation?: string;
+    message: string;
+    lastLocation?: string;
+    currStep?: string;
+    numSteps?: string;
+    first?: boolean;
+    last?: boolean;
+}
+
+export default function TutorialModal({
+    id,
+    header,
+    icon,
+    iconHeight = "40",
+    iconWidth = "40",
+    nextLocation = "",
+    lastLocation = "",
+    message,
+    currStep = "1",
+    numSteps = "1",
+    first = false,
+    last = false,
+}: TutorialModalProps) {
+
+    const currentModal = useRef<HTMLDialogElement>(null);
+
+    function handleNext() {
+        currentModal.current?.close();
+        if (!last) {
+            const nextModal = document.getElementById(nextLocation) as HTMLDialogElement | null;
+            nextModal?.showModal();
+        }
+
+    }
+
+    function handleBack() {
+        currentModal.current?.close();
+        const lastModal = document.getElementById(lastLocation) as HTMLDialogElement | null;
+        lastModal?.showModal();
+    }
+
+
+    const backButtonClass = (first) ? " hidden" : "bg-gray-200 hover:bg-gray-300 text-black rounded-lg p-2 pl-4 pr-4 mt-5";
+    const nextButtonText = (last) ? "Done" : "Next";
+    const skipButtonClass = (last) ? "hidden" : "flex justify-self-end bg-gray-400 text-white rounded-lg p-1 pl-4 pr-4 mb-5 hover:bg-gray-500";
 
     return (
         <div>
-            <dialog id="welcome-tutorial" className="rounded-lg p-7 fixed place-self-center max-w-100 max-h-200 flex-auto">
-                <button command="close" commandfor="welcome-tutorial" className="bg-gray-400 text-white rounded-lg p-1 pl-4 pr-4 flex justify-self-end mb-5 hover:bg-gray-500">Skip</button>
+            <dialog ref={currentModal} id={id} className="rounded-lg p-7 fixed place-self-center max-w-100 max-h-200 flex-auto">
+                <button onClick={() => { currentModal.current?.close() }} className={skipButtonClass}>Skip</button>
                 <h2 className="text-center text-xl">{header}</h2>
-                <Image src={icon} alt="sun" width={40}
-                    height={40} className="place-self-center m-2" />
+                <Image src={icon} alt="sun" width={Number(iconWidth)}
+                    height={Number(iconHeight)} className="place-self-center m-2" />
                 <p className="text-center m-2" >{message}</p>
-                <div className="flex flex-row justify-end gap-x-20">
-                    <button command="show-modal" commandfor={lastLocation} className={backButtonClass}>Back</button>
-                    <button command="show-modal" commandfor={nextLocation} className="bg-gray-500 hover:bg-gray-600 text-white rounded-lg p-2 pl-4 pr-4 mt-5">{nextButtonText}</button>
+                <p className="text-center text-sm mt-5">Step {currStep} of {numSteps}</p>
+                <div className="flex flex-row justify-end gap-x-50">
+                    <button onClick={() => { handleBack() }} className={backButtonClass}>Back</button>
+                    <button onClick={() => { handleNext() }} className="bg-gray-500 hover:bg-gray-600 text-white rounded-lg p-2 pl-4 pr-4 mt-5" autoFocus={true}>{nextButtonText}</button>
                 </div>
             </dialog >
         </div >
