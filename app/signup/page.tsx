b1b439d8bda72f43460cf4f2d2c26ef57aa1d5ae
celestiawana <wyang73@my.bcit.ca>
2026-05-09 02:17:35 +0800

    revise login and signup and authentication componet file

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,7 +5,7 @@ import { signUpNewUser } from "@/actions/auth"
 export default function SignupPage() {
   return (
     <AuthenticationComponent
-      title="Create Vancooler account"
+      title="Create account"
       submitBtnName="Sign up"
       successMessage="Congratulations! Your account is created"
       redirectPath="/login"
