6001ce29593a31d13ba93b985a279b1fbf59f067
celestiawana <wyang73@my.bcit.ca>
2026-05-14 01:21:04 +0800

    temp: save TutorialModal and cafe.png

--- components/TutorialModal.tsx
+++ components/TutorialModal.tsx
@@ -34,25 +34,28 @@ export default function TutorialModal({
   first = false,
   last = false,
 }: TutorialModalProps) {
+  // Create a reference for the current modal
+  const currentModal = useRef<HTMLDialogElement>(null);
 
-    // Create a reference for the current modal
-    const currentModal = useRef<HTMLDialogElement>(null);
-
-    // Handler for the Next or Done button on Click action.
-    function handleNext() {
-        currentModal.current?.close();
-        if (!last) {
-            const nextModal = document.getElementById(nextLocation) as HTMLDialogElement | null;
-            nextModal?.showModal();
-        }
+  // Handler for the Next or Done button on Click action.
+  function handleNext() {
+    currentModal.current?.close();
+    if (!last) {
+      const nextModal = document.getElementById(
+        nextLocation,
+      ) as HTMLDialogElement | null;
+      nextModal?.showModal();
     }
+  }
 
-    // Handler for the Back button on Click action.
-    function handleBack() {
-        currentModal.current?.close();
-        const lastModal = document.getElementById(lastLocation) as HTMLDialogElement | null;
-        lastModal?.showModal();
-    }
+  // Handler for the Back button on Click action.
+  function handleBack() {
+    currentModal.current?.close();
+    const lastModal = document.getElementById(
+      lastLocation,
+    ) as HTMLDialogElement | null;
+    lastModal?.showModal();
+  }
 
   const backButtonClass = first
     ? " hidden"
@@ -62,21 +65,54 @@ export default function TutorialModal({
     ? "hidden"
     : "flex justify-self-end bg-blue-400 text-white rounded-lg p-1 pl-4 pr-4 mb-1 hover:bg-blue-500";
 
-    // The HTML layout and elements of the Tutorial Modal.
-    return (
-        <div>
-            <dialog ref={currentModal} id={id} className="rounded-xl p-5 fixed place-self-center max-w-90 max-h-9.9/10 flex-auto">
-                <button onClick={() => { currentModal.current?.close() }} className={skipButtonClass}>Skip</button>
-                <h2 className="text-center text-xl">{header}</h2>
-                <Image src={icon} alt="sun" width={Number(iconWidth)}
-                    height={Number(iconHeight)} className="place-self-center mt-2 mb-2" />
-                <p className="text-center mt-2 mb-2" >{message}</p>
-                <p className="text-center text-sm mt-5">Step {currStep} of {numSteps}</p>
-                <div className="flex flex-row justify-between">
-                    <button onClick={() => { handleBack() }} className={backButtonClass}>Back</button>
-                    <button onClick={() => { handleNext() }} className="bg-gray-500 hover:bg-gray-600 text-white rounded-lg p-2 pl-4 pr-4 ml-auto" autoFocus={true}>{nextButtonText}</button>
-                </div>
-            </dialog >
-        </div >
-    );
+  // The HTML layout and elements of the Tutorial Modal.
+  return (
+    <div>
+      <dialog
+        ref={currentModal}
+        id={id}
+        className="rounded-xl p-5 fixed place-self-center max-w-90 max-h-9.9/10 flex-auto"
+      >
+        <button
+          onClick={() => {
+            currentModal.current?.close();
+          }}
+          className={skipButtonClass}
+        >
+          Skip
+        </button>
+        <h2 className="text-center text-xl">{header}</h2>
+        <Image
+          src={icon}
+          alt="sun"
+          width={Number(iconWidth)}
+          height={Number(iconHeight)}
+          className="place-self-center mt-2 mb-2"
+        />
+        <p className="text-center mt-2 mb-2">{message}</p>
+        <p className="text-center text-sm mt-5">
+          Step {currStep} of {numSteps}
+        </p>
+        <div className="flex flex-row justify-between">
+          <button
+            onClick={() => {
+              handleBack();
+            }}
+            className={backButtonClass}
+          >
+            Back
+          </button>
+          <button
+            onClick={() => {
+              handleNext();
+            }}
+            className="bg-blue-500 hover:bg-blue-400 text-white rounded-lg p-2 pl-4 pr-4 ml-auto"
+            autoFocus={true}
+          >
+            {nextButtonText}
+          </button>
+        </div>
+      </dialog>
+    </div>
+  );
 }
