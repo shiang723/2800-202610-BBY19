3e75715ae7d21502abad3f4803b6529d92535b0f
shiang723 <hanhanshiang6@gmail.com>
2026-05-13 13:34:02 -0700

    Implemented design of notifications as a polymorphic component

--- app/notification/page.tsx
+++ app/notification/page.tsx
@@ -1,5 +1,12 @@
 import Navbar from "@/components/Navbar";
+import Notification from "@/components/Notification";
 
 export default function NotificationPage() {
-    return (<Navbar />);
+    return (
+        <div>
+            <button command="show-modal" commandfor="notification" className="text-black">Click Here</button>
+            <Notification type="uv" uvIndex={12} />
+            <Navbar />
+        </div>
+    );
 }
---
+++ components/Notification.tsx
@@ -0,0 +1,175 @@
+import Image from "next/image"
+import { useState } from "react";
+
+/**
+ * Creating different props using type
+ * Adapted from Gemini 3 fast model
+ * https://gemini.google.com/app
+ */
+type BaseNotificationProps = {
+    timeOfNotif?: Date;
+    locationUser?: { longitude: number; latitude: number };
+};
+
+type SunscreenNotification = BaseNotificationProps & {
+    type: "sunscreen";
+};
+
+type HydrationNotification = BaseNotificationProps & {
+    type: "hydration";
+};
+
+type UVNotification = BaseNotificationProps & {
+    type: "uv";
+    uvIndex: number; // Mandatory only when type is "uv"
+};
+
+type NotificationProps = SunscreenNotification | HydrationNotification | UVNotification;
+
+/** End of adapted code */
+
+const lowMessage = "Wear UV protection sunglasses to help protect your eyes. Cover up your skin and use sunscreen if you are outside for more than one hour during bright, snowy days.";
+const moderateMessage = "Take precautions, such as covering up and using sunscreen, if you will be outside. Stay in shade near midday when the sun is the strongest."
+const highMessage = "Protection against sunburn is needed, reduce time in the sun between 11 am and 4 pm. Cover up, wear a hat and sunglasses, and use sunscreen."
+const veryHighMessage = "Extra care required. Unprotected skin burns quickly. Use sunscreen, seek shade, cover up, and wear protective clothing like a hat and sunglasses. Avoid the sun between 11 am and 4 pm."
+const extremeMessage = "Take all precautions. Unprotected skin can burn in minutes. Avoid sun between 11 am and 4 pm. Bright surfaces can reflext and increase UV exposure. Use sunscreen, seek shade, cover up, and wear protective clothing like a hat and sunglasses."
+
+const uvMap = new Map();
+uvMap.set(0, lowMessage);
+uvMap.set(1, lowMessage);
+uvMap.set(2, lowMessage);
+uvMap.set(3, moderateMessage);
+uvMap.set(4, moderateMessage);
+uvMap.set(5, moderateMessage);
+uvMap.set(6, highMessage);
+uvMap.set(7, highMessage);
+uvMap.set(8, veryHighMessage);
+uvMap.set(9, veryHighMessage)
+uvMap.set(10, veryHighMessage)
+const sunscreen =
+    `<
+    dialog id="notification" className="rounded-xl p-5 fixed place-self-center max-w-90 max-h-9.9/10 flex-auto">
+        <Image src="./sunscreen.svg" alt="sunscreen" width={45}
+            height={45} className="place-self-center mt-2 mb-2" />
+        <h2 className="text-center text-xl">Time to reapply sunscreen</h2>
+        <p className="text-center m-2" >It has been 2 hours. It is time to
+            reapply your sunscreen.</p>
+        <div className="flex">
+            <button type="button" command="close" commandfor="notification" className="bg-gray-700 hover:bg-gray-800 text-white rounded-lg grow p-1">Exit</button>
+        </div>
+    </dialog >`
+
+const hydration = `
+            <dialog id="notification" className="rounded-xl p-5 fixed place-self-center max-w-90 max-h-9.9/10 flex-auto">
+                <Image src="./sun.svg" alt="sun" width={45}
+                    height={45} className="place-self-center mt-2 mb-2" />
+                <h2 className="text-center text-xl">Hydration Reminder</h2>
+                <p className="text-center m-2" >The temperature is higher than 33 degrees. We suggest drinking water, juice, or an electrolyte drink.</p>
+                <div className="flex">
+                    <button className="bg-gray-500 hover:bg-gray-600 text-white rounded-xl grow p-1 pr-2 pl-2 mb-2 grow">View nearby water stations</button>
+                </div>
+                <div className="flex">
+                    <button className="bg-gray-500 hover:bg-gray-600 text-white rounded-xl grow p-1 pr-2 pl-2 mb-2 grow">View nearby cafes</button>
+                </div>
+                <div className="flex">
+                    <button className="bg-gray-500 hover:bg-gray-600 text-white rounded-xl grow p-1 pr-2 pl-2 mb-2 grow">View nearby grocery stores</button>
+                </div>
+                <div className="flex">
+                    <button type="button" command="close" commandfor="notification" className="bg-gray-700 hover:bg-gray-800 text-white rounded-lg grow p-1">Exit</button>
+                </div>
+            </dialog >`
+
+
+const uv = `
+            <dialog id="notification" className="rounded-xl p-5 fixed place-self-center max-w-90 max-h-9.9/10 flex-auto">
+
+                <h2 className="text-center text-xl">UV index right now is {String(uvIndex)}</h2>
+                <p className="text-center m-2" >{uvMessage}</p>
+
+                <div className="flex flex-row justify-center gap-5 mb-2">
+                    <div className="text-center">
+                        <Image src="./sunglasses.svg" alt="sunglasses" width={80}
+                            height={45} className="mt-2 mb-2" />
+                        <p>Sunglasses</p>
+                    </div>
+                    <div className="text-center">
+                        <Image src="./hat.svg" alt="hat" width={80}
+                            height={45} className="mt-2 mb-2" />
+                        <p>Hat</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <button type="button" command="close" commandfor="notification" className="bg-gray-700 hover:bg-gray-800 text-white rounded-lg grow p-1">Exit</button>
+                </div>
+            </dialog >`
+
+export default function Notification(props: NotificationProps) {
+    const { type, timeOfNotif } = props
+    const [open, setOpen] = useState(false);
+
+    if (!open) return null;
+
+    const uvMessage = (type == "uv" && props.uvIndex) ? ((props.uvIndex ?? 0 > 10) ? extremeMessage : uvMap.get(props.uvIndex)) : null
+
+    const openTime = timeOfNotif || null;
+    const now = new Date();
+
+    if (now.getDate() == openTime?.getDate()) {
+        setOpen(true);
+    }
+
+    return (
+        <dialog open={open} id="notification" className="rounded-xl p-5 fixed place-self-center max-w-90 max-h-9.9/10 flex-auto">
+            {/* How to render different html based on type prop. Adapted from Gemini 3 fast model: https://gemini.google.com/app    */}
+            {type === "sunscreen" && (
+                <>
+                    <Image src="./sunscreen.svg" alt="sunscreen" width={45}
+                        height={45} className="place-self-center mt-2 mb-2" />
+                    <h2 className="text-center text-xl">Time to reapply sunscreen</h2>
+                    <p className="text-center m-2" >It has been 2 hours. It is time to
+                        reapply your sunscreen.</p>
+                </>
+            )}
+            {type === "hydration" && (
+                <>
+                    <Image src="./sun.svg" alt="sun" width={45}
+                        height={45} className="place-self-center mt-2 mb-2" />
+                    <h2 className="text-center text-xl">Hydration Reminder</h2>
+                    <p className="text-center m-2" >The temperature is higher than 33 degrees. We suggest drinking water, juice, or an electrolyte drink.</p>
+                    <div className="flex">
+                        <button className="bg-gray-500 hover:bg-gray-600 text-white rounded-xl grow p-1 pr-2 pl-2 mb-2 grow">View nearby water stations</button>
+                    </div>
+                    <div className="flex">
+                        <button className="bg-gray-500 hover:bg-gray-600 text-white rounded-xl grow p-1 pr-2 pl-2 mb-2 grow">View nearby cafes</button>
+                    </div>
+                    <div className="flex">
+                        <button className="bg-gray-500 hover:bg-gray-600 text-white rounded-xl grow p-1 pr-2 pl-2 mb-2 grow">View nearby grocery stores</button>
+                    </div>
+                </>
+            )}
+            {type === "uv" && (
+                <>
+                    <h2 className="text-center text-xl">UV index right now is {String(props.uvIndex)}</h2>
+                    <p className="text-center m-2" >{uvMessage}</p>
+
+                    <div className="flex flex-row justify-center gap-5 mb-2">
+                        <div className="text-center">
+                            <Image src="./sunglasses.svg" alt="sunglasses" width={80}
+                                height={45} className="mt-2 mb-2" />
+                            <p>Sunglasses</p>
+                        </div>
+                        <div className="text-center">
+                            <Image src="./hat.svg" alt="hat" width={80}
+                                height={45} className="mt-2 mb-2" />
+                            <p>Hat</p>
+                        </div>
+                    </div>
+                </>
+            )}
+            <div className="flex">
+                <button type="button" onClick={() => setOpen(false)} className="bg-gray-700 hover:bg-gray-800 text-white rounded-lg grow p-1">Exit</button>
+            </div>
+        </dialog >
+    );
+}
+
