c86ade45da525171b57dfe903f863ff7f46cf34e
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-05-13 22:37:19 -0700

    feat: clone request URL in confirm route to embed url params when redirecting

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,14 +1,16 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
-import { redirect } from 'next/navigation'
-import { type NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-import { createClientForServerComponent } from "@/lib/supabase/server"
+// The client you created from the Server-Side Auth instructions
+import { createClientForServerComponent } from '@/lib/supabase/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
   const next = searchParams.get('next') ?? '/'
+  const redirectTo = request.nextUrl.clone()
+  redirectTo.pathname = next
 
   if (token_hash && type) {
     const supabase = await createClientForServerComponent()
@@ -18,11 +20,11 @@ export async function GET(request: NextRequest) {
       token_hash,
     })
     if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next)
+      return NextResponse.redirect(redirectTo)
     }
   }
 
-  // redirect the user to an error page with some instructions
-  redirect('/auth/auth-code-error')
+  // return the user to an error page with some instructions
+  redirectTo.pathname = '/auth/auth-code-error'
+  return NextResponse.redirect(redirectTo)
 }
