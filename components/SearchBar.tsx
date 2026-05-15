44d870262e40cba6739313b5606c00eef82d29d2
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-05-15 12:10:32 -0700

    fix: fix bugs in the profileicon component by deleting non-existing attributes

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function SearchBar() {
   return (
     <div className="flex items-center bg-white rounded-full shadow-md px-3 py-2 gap-3">
-      <ProfileIcon size={32} className="text-gray-700 shrink-0" />
+      <ProfileIcon />
       <div id="geocoderContainer"></div>
     </div>
   );
